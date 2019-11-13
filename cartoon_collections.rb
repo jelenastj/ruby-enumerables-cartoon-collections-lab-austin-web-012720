386df2c0e341f5b557b784e387f4a5a838fc793f
Jelena Stjepanovic <jelena.stjepanovic@gmail.com>
2019-11-13 02:31:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,6 +13,6 @@ end
 
 def find_the_cheese(string_array)
   cheese_types = ["cheddar", "gouda", "camembert"]
- if string_array.include?(cheese_types)
+ string_array.include?(cheese_types)
    
 end
